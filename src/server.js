d2026b6fb268b8b8c74a5244664afc6071ba1c4b
animagdum497 <magdumaniket497@gmail.com>
2026-02-10 13:02:33 +0530

    Enable CORS for Ionic app

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import paymentRoutes from "./routes/payment.js";
 
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/payment", paymentRoutes);
